b15518c926890d28a2220ef77b913e7ef0bd0431
Alex Ko <ko.e.alexander@gmail.com>
2019-09-15 15:23:01 -0400

    1

--- hello.js
+++ hello.js
@@ -1,11 +1,17 @@
 // var msg = 'Hello World';
 // console.log(msg);
 
+//npm install to generate node_modules
 //https://www.npmjs.com/package/validator
-const validator = require('validator')
-console.log(validator.isEmail('ko.e.alexander@gmail.com'))
-console.log(validator.isURL('www.google.com'))
+// const validator = require('validator')
+// console.log(validator.isEmail('ko.e.alexander@gmail.com'))
+// console.log(validator.isURL('www.google.com'))
 
+// const chalk = require('chalk')
+// console.log(chalk.inverse.bold.green('Hello!'))
 
-
+//https://www.npmjs.com/package/nodemon
+//npm install -g nodemon
+//nodemon hello.js
+//press ctrl+c to exit nodemon
 
